8c40739fb8d4828c4522c2cf5eaec315798d7f9d
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2023-01-13 17:54:00 +0200

    feat: create simple server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('I`m work');
+import * as http from 'http';
+import dotenv from 'dotenv';
+import users from "../users.db.json";
+
+dotenv.config();
+
+const PORT = Number(process.env.PORT);
+
+const server = http.createServer((req, res) => {
+	if (req.url === '/api/users' && req.method === 'GET') {
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify(users));
+	} else {
+		res.writeHead(404, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify({ message: 'Route Not Found' }));
+	}
+
+})
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
